b1836dcbb0871d3d887103155ef16c4eaf313545
arthurlee <boyal2008@gmail.com>
2016-07-14 12:54:56 +0800

    Hello Point 2 attribute poistion

---
+++ study/chapter02/HelloPoint_2.js
@@ -0,0 +1,61 @@
+
+//
+// ES6: template literals for multi line string, it is so useful.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+//
+
+// Vertex shader program
+var VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    void main() {
+        gl_Position = a_Position;
+        gl_PointSize = 10.0;
+    } 
+`;
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+    void main() {
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+`;
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element.');
+        return;
+    }
+
+    var gl = getWebGLContext(canvas, true);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders');
+        return;
+    }
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position.');
+        return;
+    }
+
+    // version 1
+    //gl.vertexAttrib3f(a_Position, 0.5, 0.0, 0.0);
+
+    // version 2
+    //gl.vertexAttrib4f(a_Position, 0.5, 0.5, 0.0, 1.0);
+
+    // version 3
+    var position = new Float32Array([0.5, 0.5, 0.0, 1.0]);
+    gl.vertexAttrib4fv(a_Position, position);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
