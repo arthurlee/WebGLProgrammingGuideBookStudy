44ef865d76840fa0fdd535db8305162a2c9e1b3e
arthurlee <boyal2008@gmail.com>
2016-07-15 11:01:51 +0800

    improve readable code

--- study/chapter02/ClickedPoints.js
+++ study/chapter02/ClickedPoints.js
@@ -79,18 +79,17 @@ function click(event, gl, canvas, a_Position) {
     var x = event.clientX;
     var y = event.clientY;
     var rect = event.target.getBoundingClientRect();
-    console.log(`x = ${x}, y = ${y}`);
-    console.log(rect);
+    //console.log(`x = ${x}, y = ${y}`);
+    //console.log(rect);
     x = ((x - rect.left) - canvas.height/2) / (canvas.height/2);
     y = (canvas.width/2 - (y - rect.top)) / (canvas.width/2);
 
-    g_points.push(x);
-    g_points.push(y);
+    g_points.push({x:x, y:y});
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     var len = g_points.length;
-    for (var i = 0; i < len; i+= 2) {
-        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i+1], 0.0);
+    for (var i = 0; i < len; i+= 1) {
+        gl.vertexAttrib3f(a_Position, g_points[i].x, g_points[i].y, 0.0);
         gl.drawArrays(gl.POINTS, 0, 1);
     }
 }
