a9e7e7513200af3d61bb1374dbfbab4d39b7402c
arthurlee <boyal2008@gmail.com>
2016-07-18 12:05:32 +0800

    colored points sample

---
+++ study/chapter02/ColoredPoints.js
@@ -0,0 +1,100 @@
+
+//
+// ES6: template literals for multi line string, it is so useful.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+//
+
+// Vertex shader program
+var VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    attribute float a_PointSize;
+    void main() {
+        gl_Position = a_Position;
+        gl_PointSize = a_PointSize;
+    } 
+`;
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+    precision mediump float;
+    uniform vec4 u_FragColor;
+    void main() {
+        gl_FragColor = u_FragColor;
+    }
+`;
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element.');
+        return;
+    }
+
+    var gl = getWebGLContext(canvas, true);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders');
+        return;
+    }
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position.');
+        return;
+    }
+
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+    if (a_PointSize < 0) {
+        console.log('Failed to get the storage location of a_PointSize.');
+        return;
+    }
+    
+    gl.vertexAttrib1f(a_PointSize, 10.0);
+
+    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+
+    // 
+    canvas.onmousedown = function(event) {
+        click(event, gl, canvas, a_Position, u_FragColor);
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+var g_points = [];
+var g_colors = [];
+
+function click(event, gl, canvas, a_Position, u_FragColor) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var rect = event.target.getBoundingClientRect();
+    //console.log(`x = ${x}, y = ${y}`);
+    //console.log(rect);
+    x = ((x - rect.left) - canvas.height/2) / (canvas.height/2);
+    y = (canvas.width/2 - (y - rect.top)) / (canvas.width/2);
+
+    g_points.push({x:x, y:y});
+
+    if (x >= 0.0 && y >= 0.0) { // first quadrant 第一象限
+        g_colors.push([1.0, 0.0, 0.0, 1.0]);
+    } else if ( x < 0.0 && y < 0.0) { // third quadrant 第三象限
+        g_colors.push([0.0, 1.0, 0.0, 1.0]);
+    } else {
+        g_colors.push([1.0, 1.0, 1.0, 1.0]);
+    }
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    var len = g_points.length;
+    for (var i = 0; i < len; i+= 1) {
+        gl.vertexAttrib3f(a_Position, g_points[i].x, g_points[i].y, 0.0); 
+        gl.uniform4f(u_FragColor, g_colors[i][0], g_colors[i][1], g_colors[i][2], g_colors[i][3]);        
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
+}
