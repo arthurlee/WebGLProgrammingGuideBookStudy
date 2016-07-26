a3c00bf1abc70c2f80deff06ce2d8480a7362d8a
arthurlee <boyal2008@gmail.com>
2016-07-26 14:02:43 +0800

    translated triangle sample

---
+++ study/chapter03/TranslatedTriangle.js
@@ -0,0 +1,114 @@
+
+//
+// ES6: template literals for multi line string, it is so useful.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+//
+
+// Vertex shader program
+var VSHADER_SOURCE = `
+    attribute vec4 a_Position;
+    attribute float a_PointSize;
+    uniform vec4 u_Translation;
+    void main() {
+        gl_Position = a_Position + u_Translation;
+        gl_PointSize = a_PointSize;
+    } 
+`;
+
+// Fragment shader program
+var FSHADER_SOURCE = `
+    precision mediump float;
+    uniform vec4 u_FragColor;
+    void main() {
+        gl_FragColor = u_FragColor;
+    }
+`;
+
+var Tx = 0.5, Ty = 0.5, Tz = 0.0;
+
+function main() {
+    var canvas = document.getElementById('webgl');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element.');
+        return;
+    }
+
+    var gl = getWebGLContext(canvas, true);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders');
+        return;
+    }
+
+    var n = initVertexBuffers(gl);
+    if ( n < 0 ) {
+        console.log('/Failed to set the positions of the vertices');
+        return;
+    }
+
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+    if (a_PointSize < 0) {
+        console.log('Failed to get the storage location of a_PointSize.');
+        return;
+    }
+    
+    gl.vertexAttrib1f(a_PointSize, 10.0);
+
+    var u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
+    if (! u_Translation) {
+        console.log('Failed to get u_Translation variable');
+        return;
+    }
+
+    gl.uniform4f(u_Translation, Tx, Ty, Tz, 0.0);
+
+    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    if (! u_FragColor) {
+        console.log('Failed to get u_FragColor variable');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    //var mode = gl.POINTS;
+    //var mode = gl.LINES;
+    //var mode = gl.LINE_STRIP;
+    //var mode = gl.LINE_LOOP;
+    var mode = gl.TRIANGLES;
+    //var mode = gl.TRIANGLES_STRIP;
+    gl.drawArrays(mode, 0, n);
+}
+
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5,
+        -0.5, -0.5,
+        0.5, -0.5
+    ]);
+    var n = 3;
+
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failed to create the buffer object');
+        return -1;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position.');
+        return;
+    }
+
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
