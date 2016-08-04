02f6cdcc4087dfde8e955f4b01b2753004e07521
arthurlee <boyal2008@gmail.com>
2016-08-04 11:53:15 +0800

    ColoredTriangle

--- study/chapter05/ColoredTriangle.js
+++ study/chapter05/ColoredTriangle.js
@@ -53,8 +53,7 @@ function main() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    //gl.drawArrays(gl.TRIANGLES, 0, n);
-    gl.drawArrays(gl.POINTS, 0, n);
+    gl.drawArrays(gl.TRIANGLES, 0, n);
 }
 
 function initVertexBuffers(gl) {
